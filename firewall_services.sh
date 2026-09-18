f4297b771b42640e1838996ded3163bdee9e419a
NIVETHA.M <nivethaarjun02@gmail.com>
2026-09-18 13:17:54 +0530

    Update firewall_services.sh

--- firewall_services.sh
+++ firewall_services.sh
@@ -33,45 +33,4 @@ sudo firewall-cmd --permanent --add-service=ssh
 
 
 # Reload firewall configuration
-sudo firewall-cmd --reload#!/bin/bash
-
-# ==========================================
-# Managing Firewall Services using firewalld
-#
-# Student Name:
-# Roll Number:
-# ==========================================
-
-
-# Check current firewall services
-
-
-
-
-# Allow HTTP service
-
-
-
-# Allow HTTPS service
-
-
-
-
-# Display enabled services
-
-
-
-
-# Remove HTTP service
-
-
-
-
-# Allow SSH permanently
-
-
-
-
-# Reload firewall configuration
-
-
+sudo firewall-cmd --reload
